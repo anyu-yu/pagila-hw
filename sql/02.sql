b1f8064795566cf6c37437651a28df00387478d1
anyu-yu <andrew.yu.x@gmail.com>
2022-02-11 10:42:07 -0800

    solution for 01, 02, 03, 05, and 08

--- sql/02.sql
+++ sql/02.sql
@@ -2,3 +2,7 @@
  * Find the actor_id of every actor whose first name starts with 'j'.
  * Order the results from low to hi.
  */
+
+SELECT actor_id
+FROM actor
+WHERE LOWER (SUBSTRING (first_name from 1 for 1)) = 'j'; 
--- sql/03.sql
+++ sql/03.sql
@@ -2,3 +2,8 @@
  * Find all last names of actors whose last names contain the letters LI (case insensitive).
  * Order results alphabetically.
  */
+
+SELECT last_name
+FROM actor
+WHERE POSITION ('li' in LOWER (last_name)) > 0
+ORDER BY last_name;
